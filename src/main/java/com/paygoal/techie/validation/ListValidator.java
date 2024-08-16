651c168e5a8d7a88c4f8b678eef577595f8e54df
Germán Furfori <german.furfori@paygoal.com.ar>
2024-08-16 10:14:52 -0300

    @Documented deleted

--- src/main/java/com/paygoal/techie/validation/ListValidator.java
+++ src/main/java/com/paygoal/techie/validation/ListValidator.java
@@ -3,13 +3,11 @@ package com.paygoal.techie.validation;
 import com.paygoal.techie.validation.impl.ListValidatorImpl;
 import javax.validation.Constraint;
 import javax.validation.Payload;
-import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Documented
 @Constraint(validatedBy = ListValidatorImpl.class)
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
--- src/main/java/com/paygoal/techie/validation/StringValidator.java
+++ src/main/java/com/paygoal/techie/validation/StringValidator.java
@@ -3,13 +3,11 @@ package com.paygoal.techie.validation;
 import com.paygoal.techie.validation.impl.StringValidatorImpl;
 import javax.validation.Constraint;
 import javax.validation.Payload;
-import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Documented
 @Constraint(validatedBy = StringValidatorImpl.class)
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.FIELD, ElementType.PARAMETER})
